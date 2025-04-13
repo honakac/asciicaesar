a0cb76d9fde2d97744ad2405ff82866cc8f5120b
Andrey Stekolnikov <honakac@yandex.ru>
2025-04-13 13:48:47 +0500

    Update asciicaesar.h

--- src/include/asciicaesar.h
+++ src/include/asciicaesar.h
@@ -1,5 +1,5 @@
 /*
- * BrainGear - Caesar cipher utility
+ * asciicaesar - Caesar cipher utility
  * Copyright (C) 2025 Andrey Stekolnikov <honakac@yandex.ru>
  *
  * This program is free software: you can redistribute it and/or modify
